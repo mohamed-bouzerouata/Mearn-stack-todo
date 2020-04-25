e0d02d980f95b77a4e8a400afb883fd51a2cbb63
mohamed-bouzerouata <mohameddevelopper3@gmail.com>
2020-04-25 12:05:37 +0100

    finish the app

--- Backend/server.js
+++ Backend/server.js
@@ -1,45 +1,72 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const todoRoutes = express.Router();
 const PORT = 4000;
 
-const app = express();
-const todoRoutes = express.Router();
+let Todo = require('./todo.model');
 
+app.use(cors());
 
-app.use('/todos',  todoRoutes);
-// this endpoint will delivering all available todos items :
-todoRoutes.route('/').get((req, res) => {
-    Todo.find((err, todos) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.json(todos);
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true } );
+
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+todoRoutes.route('/').get(function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(todos);
         }
-    });  
+    });
 });
-//  this is the second endpoint 
-//  we retrieve todos by providinf an ID .
-todoRoutes.route('/id').get((req, res) => {
+todoRoutes.route('/:id').get(function(req, res) {
     let id = req.params.id;
-    Todo.findById(id, (err, todo)=> {
+    Todo.findById(id, function(err, todo) {
         res.json(todo);
     });
 });
 
+todoRoutes.route('/update/:id').post(function(req, res) {
+    Todo.findById(req.params.id, function(err, todo) {
+        if (!todo)
+            res.status(404).send("data is not found");
+        else
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+            todo.save().then(todo => {
+                res.json('Todo updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
 
+todoRoutes.route('/add').post(function(req, res) {
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+        });
+});
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use('/todos', todoRoutes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', {
-    useNewUrlParser: true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('MongoDB database connection is ready')
-})
-
-app.listen(PORT, () => {
-    console.log(`the server start on the port ${PORT}`)
-})
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
